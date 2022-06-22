353c8d5bcb1b1876db0115b9897ad4d58a9f01a1
Eneko Alonso <eneko.alonso@gmail.com>
2022-06-22 23:18:11 +0200

    Font links

--- Sources/Visualizations/Font.swift
+++ Sources/Visualizations/Font.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 extension Font {
+    /// https://www.dafont.com/es/conthrax.font
     static func conthrax(_ size: CGFloat) -> Font {
         .custom("Conthrax", size: size)
     }
+    /// https://www.dafont.com/es/good-timing.font
     static func goodTiming(_ size: CGFloat) -> Font {
         .custom("Good Timing", size: size)
     }
+    /// https://www.dafont.com/es/terminator.font
     static func terminator(_ size: CGFloat) -> Font {
         .custom("Terminator", size: size)
     }
